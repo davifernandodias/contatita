d8497b72da1446c645679a58f92f504d20069d3b
Davi Dias <davifernandosousadias18@gmail.com>
2026-02-25 09:54:43 -0300

    feat(devops): corrections to the environments and changes to the vite config to use the proxy for communication with the API

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { join } from 'path';
 
-export default defineConfig({
-  plugins: [tailwindcss() ,react()],
-  resolve: {
-    alias: {
-      "@": join(import.meta.dirname, "src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [tailwindcss(), react()],
+    resolve: {
+      alias: {
+        "@": join(import.meta.dirname, "src"),
+      },
     },
-  }
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
