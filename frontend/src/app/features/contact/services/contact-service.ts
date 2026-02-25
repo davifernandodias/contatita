ee13e2af35a78617c88882c3535411f32d19c44b
Davi Dias <davifernandosousadias18@gmail.com>
2026-02-25 09:58:23 -0300

    feat(frontend): international phone number validation has been implemented in the contact registration service

--- frontend/src/app/features/contact/services/contact-service.ts
+++ frontend/src/app/features/contact/services/contact-service.ts
@@ -2,13 +2,25 @@ import { httpClient } from '@/app/core/api/http-client'
 import { API_ENDPOINTS } from '@/app/shared/constants/api-endpoints'
 import { Contact } from '@/app/types/contact'
 import { ContactSearchParams } from '../types/contact-search-params'
+import { ContactResponse } from '../types/contact-register-form'
+import { normalizePhone } from '@/utils/phone-validator'
 
 export const contactService = {
-  register: async (contact: Contact): Promise<Contact> => {
+  register: async (contact: Contact): Promise<ContactResponse> => {
+    // Sanatiza os telefones
+    const normalizedPhones =
+      contact.phones?.map((p) => ({
+        ...p,
+        numero: normalizePhone(p.numero)
+      })) ?? null
+
+    const payload = { ...contact, phones: normalizedPhones }
+
     const { data } = await httpClient.post(
       API_ENDPOINTS.CONTACT.REGISTER,
-      contact
+      payload
     )
+
     return data
   },
 
